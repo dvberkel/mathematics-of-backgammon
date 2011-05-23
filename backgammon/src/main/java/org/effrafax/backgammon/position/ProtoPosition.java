1db3a1e9deaba0a2141c36d2863093d13e5d7038
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-05-23 16:09:01 +0200

    ProtoPosition added
    A test and the implentation for a protoposition is added

---
+++ backgammon/src/main/java/org/effrafax/backgammon/position/ProtoPosition.java
@@ -0,0 +1,30 @@
+package org.effrafax.backgammon.position;
+
+public class ProtoPosition
+{
+	private int[] protoPosition;
+
+	public ProtoPosition(int... protoPosition)
+	{
+		this.protoPosition = protoPosition;
+	}
+
+	public int numberOfPoints()
+	{
+		return countNumberOf(0) + 1;
+	}
+
+	private int countNumberOf(int targetElement)
+	{
+		int count = 0;
+		for (int element : protoPosition)
+		{
+			if (targetElement == element)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
---
+++ backgammon/src/test/java/org/effrafax/backgammon/position/ProtoPositionTest.java
@@ -0,0 +1,22 @@
+package org.effrafax.backgammon.position;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ProtoPositionTest
+{
+	@Test
+	public void canBeConstructed()
+	{
+		assertNotNull(new ProtoPosition(0, 1, 0, 1));
+	}
+
+	@Test
+	public void canDetermineNumberOfPoints()
+	{
+		ProtoPosition protoPosition = new ProtoPosition(0, 1, 0, 1);
+		assertTrue(3 == protoPosition.numberOfPoints());
+	}
+}
